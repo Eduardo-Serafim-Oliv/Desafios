4d84db4ad51d61d886b1bb4847f144cf2bfef536
Eduardo Serafim de Oliveira <eduardo.serafim@academico.ifg.edu.br>
2024-11-13 11:53:31 -0300

    PrimeiraTela Login

--- src/main/java/com/mycompany/projetoum/ProjetoUm.java
+++ src/main/java/com/mycompany/projetoum/ProjetoUm.java
@@ -12,6 +12,6 @@ package com.mycompany.projetoum;
 public class ProjetoUm {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       TelaDeLogin tela = new TelaDeLogin();
     }
 }
